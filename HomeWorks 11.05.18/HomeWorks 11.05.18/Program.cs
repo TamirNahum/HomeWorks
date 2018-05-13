dacc00f07a7e2c000e7d3e48d4d391853bddb506
TamirNahum <35562990+TamirNahum@users.noreply.github.com>
2018-05-13 17:37:54 +0300

    HomeWorks 11.05.18
    HomeWorks 11.05.18

---
+++ HomeWorks 11.05.18/HomeWorks 11.05.18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks_11._05._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 0;
+
+            Console.WriteLine("please first number");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("please second number");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("{0}-{1}=",num1,num2);
+            Console.WriteLine(num1-num2);
+
+
+        }
+    }
+}
