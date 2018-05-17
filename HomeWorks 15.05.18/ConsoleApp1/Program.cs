0e4e9a2a81e22f8f47cd5cd2c69c9f421d693679
TamirNahum <35562990+TamirNahum@users.noreply.github.com>
2018-05-17 13:34:50 +0300

    HomeWorks 15.05.18
    HomeWorks 15.05.18

---
+++ HomeWorks 15.05.18/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[8];
+            int prev = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("enter a number");
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i < arr.Length - 1)
+                {
+                   
+                
+                   
+                if ((arr[i]+arr[i+1])%2==1)
+                {
+                    Console.WriteLine($"{arr[i]},{arr[i+1]}");
+                }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
