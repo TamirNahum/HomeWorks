ec6b68aa38af488085e3b4a5b6e0a2ed50c8d9ce
TamirNahum <35562990+TamirNahum@users.noreply.github.com>
2018-06-07 12:03:16 +0300

    HomeWorks 05.06.18
    HomeWorks 05.06.18

---
+++ HomeWorks 05.06.18/HomeWorks 05.06.18/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks_05._06._18
+{
+    public delegate string ActionHandler(string str);
+
+
+    public class Person
+    {
+        public event ActionHandler Event1;
+
+
+        public string FullName { get; set; }
+        public bool WantToSign { get; set; }
+
+        public string SignAction()
+        {
+            if (WantToSign == true)
+            {
+                if (Event1 != null)
+                {
+                    return Event1(FullName);
+                }
+            }
+            return null;
+        }
+        }
+    }
---
+++ HomeWorks 05.06.18/HomeWorks 05.06.18/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks_05._06._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] peopleArray=new Person[7];
+            Signature[] signatureArray=new Signature[7];
+
+            peopleArray[0] = new Person { FullName = "Bob Arnold", WantToSign = true };
+            peopleArray[1] = new Person { FullName = "Alice Avery", WantToSign = true };
+            peopleArray[2] = new Person { FullName = "Albert Bailey", WantToSign = true };
+            peopleArray[3] = new Person { FullName = "Matt Baker", WantToSign = false };
+            peopleArray[4] = new Person { FullName = "Carol Campbell", WantToSign = true };
+            peopleArray[5] = new Person { FullName = "Kevin Brown", WantToSign = false };
+            peopleArray[6] = new Person { FullName = "Lisa Bower", WantToSign = true };
+
+            peopleArray[0].Event1 += (str) => peopleArray[0].FullName.ToLower();
+            peopleArray[1].Event1 += (str) => "AliceAvery";
+            peopleArray[2].Event1 += (str) => "A B";
+            peopleArray[3].Event1 += (str) => "M Baker";
+            peopleArray[4].Event1 += (str) => peopleArray[4].FullName.ToUpper();
+            peopleArray[5].Event1 += (str) => "Kelvin.Brown";
+            peopleArray[6].Event1 += (str) => "lisa";
+
+
+            for (int i = 0; i < peopleArray.Length; i++)
+            {
+                signatureArray[i]=new Signature(peopleArray[i].FullName , peopleArray[i].SignAction());
+            }
+
+            foreach (Signature s in signatureArray)
+            {
+                Console.WriteLine($"{s.FullName}, {s.FullSignature}");
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
