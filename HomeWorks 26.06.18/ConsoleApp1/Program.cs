7939fb2af4228809fc5869f91c0e220e4db62021
TamirNahum <35562990+TamirNahum@users.noreply.github.com>
2018-06-27 22:02:49 +0300

    HomeWorks 26.06.18
    HomeWorks 26.06.18

---
+++ HomeWorks 26.06.18/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NorthwindEntities ef = new NorthwindEntities())
+            {
+
+                foreach (Category c in ef.Categories)
+                {
+                    Console.WriteLine($"products with the same count of name letters as in{c.CategoryName}  :");
+                    foreach (Product p in c.Products.Where(p => p.ProductName.Length ==c.CategoryName.Length))
+                    {
+                        Console.Write($"{p.ProductName}, ");
+                        Console.Write($"{p.UnitPrice}, ");
+                        p.UnitPrice=p.UnitPrice-p.UnitPrice/10;
+                        Console.Write($"{p.UnitPrice}, ");
+
+                    }
+                    Console.WriteLine("\n_____________________________________________________________");
+                }
+
+
+                ef.SaveChanges();
+                Console.Read();
+            }
+        }
+    }
+}
